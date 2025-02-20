073a26f0b4eb86c5c4309e6cf4f40a419bfca96a
Ben Turner <turner.1893@buckeyemail.osu.edu>
2025-02-20 18:12:45 -0500

    Modified X Axis Controller Code
    * Not a finalized version
    * adds global variables for X, Z, and pitch angle

--- rceti_controller/rceti_controller/rceti_robot_controller.py
+++ rceti_controller/rceti_controller/rceti_robot_controller.py
@@ -8,27 +8,33 @@ class RCETIRobotController(Node):
 
     def __init__(self):
         super().__init__('rceti_controller')
-        self.x_position = self.create_subscription(
+        self.x_position_sub = self.create_subscription(
             Float32,
             'rceti/x_position',
             self.x_position_callback,
             10)
-        self.z_position = self.create_subscription(
+        self.z_position_sub = self.create_subscription(
             Float32,
             'rceti/z_position',
             self.z_position_callback,
             10)
-        self.pitch_angle = self.create_subscription(
+        self.pitch_angle_sub = self.create_subscription(
             Float32,
             'rceti/pitch_angle',
             self.pitch_angle_callback,
             10)
-        self.x_position # prevent unused variable warning
-        self.z_position
-        self.pitch_angle
+        self.x_position_sub # prevent unused variable warning
+        self.z_position_sub
+        self.pitch_angle_sub
+        self.x_position = 0
+        self.z_position = 0
+        self.pitch_angle = 0
         
     def x_position_callback(self, msg):
-        self.get_logger().info('I am going to x:"%s"' % msg.data)
+        if(self.x_position != msg.data):
+            print("Moving X to " + str(msg.data))
+            self.x_position = msg.data
+            # THIS IS WHERE THE X POSITION ON THE ROBOT SIDE WILL MOVE THE ROBOT
         
     def z_position_callback(self, msg):
         print("Hi")
