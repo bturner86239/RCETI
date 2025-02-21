aff2d3145b30f7e88e00272fef1da83e347eb944
Preston Wilder <pawilder11@gmail.com>
2025-02-21 14:38:02 -0500

    Robot Code
    Changed pi side of Ros to interact with keyboard correctly

--- rceti_controller/rceti_controller/rceti_robot_controller.py
+++ rceti_controller/rceti_controller/rceti_robot_controller.py
@@ -1,13 +1,14 @@
 import rclpy
 from rclpy.node import Node
-
 from std_msgs.msg import Float32
-
+import time  # for delays/testing
+import gpiozero  # motor control
 
 class RCETIRobotController(Node):
 
     def __init__(self):
         super().__init__('rceti_controller')
+        
         self.x_position_sub = self.create_subscription(
             Float32,
             'rceti/x_position',
@@ -23,45 +24,59 @@ class RCETIRobotController(Node):
             'rceti/pitch_angle',
             self.pitch_angle_callback,
             10)
-        self.x_position_sub # prevent unused variable warning
+
+        #prevent unused variable warning
+        self.x_position_sub  
         self.z_position_sub
         self.pitch_angle_sub
-        self.x_position = 0
-        self.z_position = 0
-        self.pitch_angle = 0
-        
-    def x_position_callback(self, msg):
-        if(self.x_position != msg.data):
-            print("Moving X to " + str(msg.data))
-            self.x_position = msg.data
-            # THIS IS WHERE THE X POSITION ON THE ROBOT SIDE WILL MOVE THE ROBOT
-        
-    def z_position_callback(self, msg):
-        print("Hi")
-        # THIS IS WHERE THE Z POSITION ON THE ROBOT SIDE WILL MOVE THE ROBOT
-        
-        ## TODO ##
-        
-    def pitch_angle_callback(self, msg):
-        print("Hi")
-        # THIS IS WHERE THE PITCH ANGLE ON THE ROBOT SIDE WILL MOVE THE ROBOT
-        
-        ## TODO ##
 
+        #position Tracking
+        self.x_position = 0.0
+        self.z_position = 0.0
+        self.pitch_angle = 0.0
+
+        #GPIO motors
+        self.x_Axis = gpiozero.Motor(20, 21)  
+        self.z_Axis = gpiozero.Motor(18, 19)  
+
+    def x_position_callback(self, msg):
+        if self.x_position != msg.data:
+            self.get_logger().info(f"Moving X to {msg.data}")
+            self.x_position = msg.data
+
+            if msg.data < 0:  #left when A
+                self.x_Axis.backward()
+            elif msg.data > 0:  #right when D
+                self.x_Axis.forward()
+            else:
+                self.x_Axis.stop()
+
+    def z_position_callback(self, msg):
+        if self.z_position != msg.data:
+            self.get_logger().info(f"Moving Z to {msg.data}")
+            self.z_position = msg.data
+
+            if msg.data > 0:  #up when W
+                self.z_Axis.forward()
+            elif msg.data < 0:  #down when S
+                self.z_Axis.backward()
+            else:
+                self.z_Axis.stop()  
+
+    def pitch_angle_callback(self, msg):
+        """Adjust robot pitch (for tilting platforms, arms, etc.)"""
+        if self.pitch_angle != msg.data:
+            self.get_logger().info(f"Adjusting Pitch to {msg.data}")
+            self.pitch_angle = msg.data
+            # TODO: Implement pitch 
 
 def main(args=None):
     rclpy.init(args=args)
+    robot_controller = RCETIRobotController()
+    rclpy.spin(robot_controller)
 
-    minimal_subscriber = RCETIRobotController()
-
-    rclpy.spin(minimal_subscriber)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    minimal_subscriber.destroy_node()
+    robot_controller.destroy_node()
     rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
